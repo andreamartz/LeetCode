2d0ccd5aa7fcc9d8584c14161a9851f46648cb87
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2023-02-17 18:09:31 -0600

    Complete lc-1290-convert-binary-number-in-a-linked-list(js)

---
+++ lc-1290-convert-binary-number-in-a-linked-list-to-integer/index.js
@@ -0,0 +1,70 @@
+// https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+
+// 1290. Convert Binary Number in a Linked List to Integer
+// Easy
+// 3.4K
+// 145
+// Oracle
+// JPMorgan
+// company
+// Amazon
+// Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+// Return the decimal value of the number in the linked list.
+
+// The most significant bit is at the head of the linked list.
+
+
+// Example 1:
+
+// Input: head = [1,0,1]
+// Output: 5
+// Explanation: (101) in base 2 = (5) in base 10
+
+
+// Example 2:
+
+// Input: head = [0]
+// Output: 0
+ 
+
+// Constraints:
+
+// The Linked List is not empty.
+// Number of nodes will not exceed 30.
+// Each node's value is either 0 or 1.
+
+
+
+/**
+* Definition for singly-linked list.
+* function ListNode(val, next) {
+*     this.val = (val===undefined ? 0 : val)
+*     this.next = (next===undefined ? null : next)
+* }
+*/
+/**
+* @param {ListNode} head
+* @return {number}
+*/
+
+var getDecimalValue = function(head) {
+  const values = [head.val];
+  let curr = head;
+
+  while(curr.next) {
+      values.push(curr.next.val);
+      curr = curr.next;
+  }
+
+  let power = values.length - 1;
+  let decimal = 0;
+
+  for (let value of values) {
+      if (power >= 0) {
+          decimal += (value * (2 ** power)); 
+          power -= 1;
+      }
+  }
+  return decimal;
+}
